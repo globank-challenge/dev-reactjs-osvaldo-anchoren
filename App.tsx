7bf0dfdeade09059af06bcd59d55c3a78b2bc849
GLOBANT\osvaldo.anchoren <osvaldo.anchoren@globant.com>
2022-07-12 15:42:00 -0500

    fix the bloated code in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,71 +1,15 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
+ * Pokedex app
+ * 
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React from 'react';
 
-import {
-  Colors,
-  DebugInstructions,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { BasicLayout, Header, PokemonList } from './components'; 
 
-import { BasicLayout, Header, Pokemon } from './components';
-import { PokemonList } from './components/PokemonList';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <BasicLayout>
       <Header />
@@ -73,24 +17,5 @@ const App = () => {
     </BasicLayout>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- components/Pokemon.tsx
+++ components/Pokemon.tsx
@@ -1,16 +1,19 @@
 import React, {type PropsWithChildren, type FC} from 'react';
-import { Button, Card, Title, Paragraph } from 'react-native-paper';
+import { Card, Title, Paragraph, Divider } from 'react-native-paper';
 
 
 
 export const Pokemon:FC<PropsWithChildren<{title: string, desc: string, image: string}>>  = ({title, desc, image}) => {
     return (
-        <Card>
-            <Card.Content>
-                <Title>{title}</Title>
-                <Paragraph>{desc}</Paragraph>
-            </Card.Content>
-            <Card.Cover source={{ uri: image }} />
-        </Card>
+        <>
+            <Card>
+                <Card.Content>
+                    <Title>{title}</Title>
+                    <Paragraph>{desc}</Paragraph>
+                </Card.Content>
+                <Card.Cover source={{ uri: image }} />
+            </Card>
+            <Divider />
+        </>
     )};
 
