0642dfd77a5b19c6fb096d28aecd0eb0994f18e9
GLOBANT\osvaldo.anchoren <osvaldo.anchoren@globant.com>
2022-07-12 16:54:25 -0500

    added simple pagination

--- api/pokeApi.ts
+++ api/pokeApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const pokeApi = axios.create({
-    baseURL: 'https://pokeapi.co/api/v2/'
+    baseURL: 'https://pokeapi.co/api/v2'
   });
 
--- components/PokemonList.tsx
+++ components/PokemonList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, type FC } from 'react';
 import { View, Text } from 'react-native';
-import { Snackbar } from 'react-native-paper';
+import { Snackbar, Button } from 'react-native-paper';
 import { pokeApi } from '../api';
 
 import { Pokedex, PokemonListType } from '../interfaces';
@@ -10,27 +10,42 @@ import { Pokemon } from './Pokemon';
 export const PokemonList:FC  = ({}) => {
     const [pokemons, setPokemons] = useState<PokemonListType[]>([])
     const [snackbarVisible, setSnackbarVisible] = useState(false)
+    const [loading, setLoading] = useState(true)
+
+    const [limit, setLimit] = useState(20);
+    const [offset, setOffset] = useState(0);
 
     useEffect(() => {
-      pokeApi.get<Pokedex>('/pokemon?limit=100000&offset=0').then( ({data}) => {
+        pokeApi.get<Pokedex>(`/pokemon?limit=${limit}&offset=${offset}`).then( ({data}) => {
         const newPokemons: PokemonListType[] = data.results.map((p, index) => {return {
             name: p.name,
-            desc: `Es el pokemon numero ${index+1}, y se llama ${p.name}`,
-            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${index+1}.png`
+            desc: `Es el pokemon número ${pokemons.length + index + 1}, y se llama ${p.name}`,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemons.length + index + 1}.png`
         }})
-        setPokemons(newPokemons);
+        setPokemons([...pokemons , ...newPokemons]);
+        setLoading(false);
+        console.log(pokemons);
       }).catch(err => {
+        console.log(JSON.stringify(err));
         setSnackbarVisible(true);
       })
-    }, [])
+    }, [offset])
+
+    const handleMore = ()  => {
+        setOffset(offset + limit);
+        setLoading(true);
+    }
     
 
     return (
         <View>
-            {!pokemons.length ? (<Loading />) : null}
             {pokemons.map(({name, desc, image}) => (
                 <Pokemon key={name} title={name} desc={desc} image={image} />
             ))}
+            {loading ? (<Loading />) : null}
+
+            {!loading ? (<Button onPress={handleMore}>Más Pokemons!</Button>) : null}
+            
             <Snackbar
                 visible={snackbarVisible}
                 onDismiss={() => {}}
